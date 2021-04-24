580094c26a2a90cce3083bff6784ea5f44231635
tirth0 <tirtharajsengupta@gmail.com>
2021-04-24 18:59:28 +0530

    Updates

---
+++ precache-manifest.b9c29d0d6652a5e9aff000f9aa510621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92975b56afb1d13bfe16b3ef065886c7",
+    "url": "/funwflags/index.html"
+  },
+  {
+    "revision": "29ce6b8a3a335b849c26",
+    "url": "/funwflags/static/css/main.3c3febbd.chunk.css"
+  },
+  {
+    "revision": "ab1ba624b4348187162d",
+    "url": "/funwflags/static/js/2.9af6c336.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funwflags/static/js/2.9af6c336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ce6b8a3a335b849c26",
+    "url": "/funwflags/static/js/main.b01a8203.chunk.js"
+  },
+  {
+    "revision": "3c48b220862428408698",
+    "url": "/funwflags/static/js/runtime-main.0e80df2f.js"
+  }
+]);
